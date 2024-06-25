e0480df7eb239a164af2c996e76322743e465558
mykola <mykola@kali.kali>
2024-06-25 20:03:44 +0300

    Create joke

--- main.py
+++ main.py
@@ -4,3 +4,13 @@ print(0o123,0x123)
 print(.4)
  
 print(1010)
+
+tank = 'Abrams M1'
+cost = "4500 000$"
+damage_per_second = 400
+print("Tank abilities:",tank,cost,damage_per_second)
+
+a = 3.0
+b = 4.0
+c = (a ** 2 + b ** 2) ** 0.5
+print("c =", c)
